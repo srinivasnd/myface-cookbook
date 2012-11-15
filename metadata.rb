7b4fdcea4aa1d8d3f05efeb6207a054610f9bc1e
Sean OMeara <someara@opscode.com>
2012-11-14 16:50:00 -0800

    initial database working

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ description      "Installs/Configures myface"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends "mysql"
+depends "database"
+
+
+
