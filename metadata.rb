9a4d40cb8fbdf13c779cc3d616caaf21904795d8
Sean OMeara <someara@opscode.com>
2012-11-15 18:39:03 -0800

    first fully working version of myface

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,14 @@ maintainer_email "someara@opscode.com"
 license          "Apache 2"
 description      "Installs/Configures myface"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "mysql"
 depends "database"
+depends "apache2"
+depends "php"
+
+
 
 
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -32,3 +32,38 @@ execute "initialize myface database" do
   not_if "mysql -h localhost -u root -p#{node['mysql']['server_root_password']} -D myface -e 'describe users;'"
 end
 
+###################
+# webserver section
+###################
+
+include_recipe "apache2"
+include_recipe "apache2::mod_php5"
+include_recipe "php::module_mysql"
+
+# disable default site
+apache_site "000-default" do
+  enable false
+end
+
+# create document root
+directory  "/srv/apache/myface" do
+  action :create
+  recursive true
+end
+
+# create apache config
+template "#{node['apache']['dir']}/sites-available/myface.conf" do
+  source "apache2.conf.erb"
+  notifies :restart, 'service[apache2]'
+end
+
+# enable myface
+apache_site "myface.conf" do
+  enable true
+end
+
+# write site
+template "/srv/apache/myface/index.php" do
+  source "index.php.erb"
+  mode "644"
+end
